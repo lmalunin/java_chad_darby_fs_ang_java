7246fab9fd93a23ac8431a2709bc4151d27b8309
lmalunin <lmalunin@gmail.com>
2022-04-02 16:43:52 +0300

    12.4

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
@@ -7,12 +8,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+const routes: Routes =[
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full'},
+  { path: '**', redirectTo: '/products', pathMatch: 'full'},
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
     FontAwesomeModule
