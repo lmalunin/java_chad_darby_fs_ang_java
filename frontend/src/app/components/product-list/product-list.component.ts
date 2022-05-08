6c4d7581e0202352246e1faa1f2f22cec85a3132
lmalunin <lmalunin@gmail.com>
2022-05-08 16:02:42 +0300

    14.7

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -4,40 +4,59 @@ import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list-grid.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: 'app-product-list',
+    templateUrl: './product-list-grid.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[];
-  currentCategoryId: number;
-  currentCategoryName: string;
+    products: Product[];
+    currentCategoryId: number;
+    currentCategoryName: string;
+    searchMode: boolean;
 
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(_ => {
-      this.listProducts();
-    })
-  }
-
-  listProducts() {
-    const hasCategoryId: boolean = this.activatedRoute.snapshot.paramMap.has('id');
-
-    if(hasCategoryId){
-      this.currentCategoryId = +this.activatedRoute.snapshot.paramMap.get('id');
-      this.currentCategoryName = this.activatedRoute.snapshot.paramMap.get('name');
-    }else{
-      this.currentCategoryId = 1;
-      this.currentCategoryName = 'Books';
+    constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) {
     }
 
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
-  }
+    ngOnInit() {
+        this.activatedRoute.paramMap.subscribe(_ => {
+            this.listProducts();
+        })
+    }
 
+    listProducts() {
+        this.searchMode = this.activatedRoute.snapshot.paramMap.has('keyword')
+        if (this.searchMode) {
+            this.handleSearchProducts();
+        } else {
+            this.handleListProducts()
+        }
+    }
+
+    handleListProducts() {
+        const hasCategoryId: boolean = this.activatedRoute.snapshot.paramMap.has('id');
+
+        if (hasCategoryId) {
+            this.currentCategoryId = +this.activatedRoute.snapshot.paramMap.get('id');
+            this.currentCategoryName = this.activatedRoute.snapshot.paramMap.get('name');
+        } else {
+            this.currentCategoryId = 1;
+            this.currentCategoryName = 'Books';
+        }
+
+        this.productService.getProductList(this.currentCategoryId).subscribe(
+            data => {
+                this.products = data;
+            }
+        )
+    }
+
+    private handleSearchProducts() {
+        const theKeyword: string = this.activatedRoute.snapshot.paramMap.get('keyword');
+        this.productService.searchProducts(theKeyword).subscribe(
+            data => {
+                this.products = data;
+            }
+        );
+    }
 }
