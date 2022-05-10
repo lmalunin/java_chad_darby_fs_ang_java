f007c0df2d18cb3834ea0e46798d76161469fe9c
lmalunin <lmalunin@gmail.com>
2022-05-10 10:58:43 +0300

    16.6

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -10,10 +10,14 @@ import { Product } from 'src/app/common/product';
 })
 export class ProductListComponent implements OnInit {
 
-    products: Product[];
-    currentCategoryId: number;
-    currentCategoryName: string;
-    searchMode: boolean;
+    products: Product[] = [];
+    currentCategoryId: number = 1;
+    previousCategoryId: number = 1;
+    currentCategoryName: string = '';
+    searchMode: boolean = false;
+    thePageNumber: number = 1;
+    thePageSize: number = 1;
+    theTotalElements: number = 0;
 
     constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) {
     }
@@ -44,11 +48,16 @@ export class ProductListComponent implements OnInit {
             this.currentCategoryName = 'Books';
         }
 
-        this.productService.getProductList(this.currentCategoryId).subscribe(
-            data => {
-                this.products = data;
-            }
-        )
+        if (this.previousCategoryId != this.currentCategoryId) {
+            this.thePageNumber = 1;
+        }
+        this.previousCategoryId = this.currentCategoryId;
+
+        console.log(`currentCategoryId = ${this.currentCategoryId}, thePageNumber = ${this.thePageNumber}`);
+
+        this.productService.getProductListPaginate(this.thePageNumber - 1, this.thePageSize, this.currentCategoryId).subscribe(
+            this.processResult()
+        );
     }
 
     private handleSearchProducts() {
@@ -58,5 +67,14 @@ export class ProductListComponent implements OnInit {
                 this.products = data;
             }
         );
+    }
+
+    private processResult() {
+        return (data: { _embedded: { products: Product[]; }; page: { number: number; size: number; totalElements: number; }; }) => {
+            this.products = data._embedded.products;
+            this.thePageNumber = data.page.number + 1;
+            this.thePageSize = data.page.size;
+            this.theTotalElements = data.page.totalElements;
+        }
     }
 }
