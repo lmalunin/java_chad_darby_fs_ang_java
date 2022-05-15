128a797ac32540b3e7e4e9fd69bfec7c5c1317cb
lmalunin <lmalunin@gmail.com>
2022-05-15 14:21:11 +0300

    17.14

--- frontend/src/app/components/product-details/product-details.component.ts
+++ frontend/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from '../../common/cart-item';
 import { Product } from '../../common/product';
+import { CartService } from '../../services/cart.service';
 import { ProductService } from '../../services/product.service';
 
 @Component({
@@ -12,13 +14,21 @@ export class ProductDetailsComponent implements OnInit {
 
     product: Product;
 
-    constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) {
+    constructor(private productService: ProductService, private activatedRoute: ActivatedRoute, private _cartService: CartService) {
     }
 
     ngOnInit(): void {
         this.activatedRoute.paramMap.subscribe(() => {
             this.handleProductDetails();
         });
+    }
+
+    addToCart() {
+        console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+
+        const theCartItem = new CartItem(this.product);
+
+        this._cartService.addToCart(theCartItem);
     }
 
     private handleProductDetails() {
