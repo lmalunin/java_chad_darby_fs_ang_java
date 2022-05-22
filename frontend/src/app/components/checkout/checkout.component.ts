6633d5573c608086ca94bee49c3d99fc47ab3722
lmalunin <lmalunin@gmail.com>
2022-05-22 16:03:13 +0300

    21.3

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from '../../common/country';
 import { State } from '../../common/state';
 import { Luv2ShopFormService } from '../../services/luv2-shop-form.service';
@@ -30,9 +30,9 @@ export class CheckoutComponent implements OnInit {
     ngOnInit(): void {
         this.checkoutFormGroup = this._formByilder.group({
             customer: this._formByilder.group({
-                firstName: [''],
-                lastName: [''],
-                email: ['']
+                firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+                lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+                email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
             }),
             shippingAddress: this._formByilder.group({
                 street: [''],
