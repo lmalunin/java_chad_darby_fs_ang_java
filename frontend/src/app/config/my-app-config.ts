10cea402a94754fde7179017f314defb69091532
lmalunin <lmalunin@gmail.com>
2022-05-27 15:55:20 +0300

    25.9

---
+++ frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa579xqesdR4Qf8R5d7',
+        issuer: 'https://dev-11679924.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
